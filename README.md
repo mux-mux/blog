1c098e45340c1b9f17c5dcbd7b15d27d9830aaee
mux-mux <kobylyukh@gmail.com>
2025-03-09 21:03:22 +0200

    my-update-homepage-to-show-all-posts

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 ## About This Project
 
-This project was created as part of the **Joy of React** course by Josh W. Comeau. It was built to practice and apply key React.js and Next.js concepts.
+This project was created as part of the **Joy of React** course by Josh W. Comeau. It was built to practice and apply key **React.js** and **Next.js** concepts.
+
+## Getting Started
+
+We begin with pre-built components and styles, but without functionality. The goal is to bring this project to life.
+
+## Prerequiremnts
+
+1️⃣ First, I attempt to solve each task on my own.<br />
+2️⃣ Then, I review Josh’s approach to see how he would tackle it.<br />
+3️⃣ Finally, I take notes on what can be improved.
+
+## Task 1 - update the homepage so that it shows a list of blog posts
+
+//TODO Iterate over the data read from the file system!
+Problem: `"fs" module not found` error when using `setState`
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,34 @@
-import React from 'react';
-
+'use client';
+import { useEffect, useState } from 'react';
 import BlogSummaryCard from '@/components/BlogSummaryCard';
+import { getBlogPostList } from '@/helpers/file-helpers';
 
 import styles from './homepage.module.css';
 
 function Home() {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  useEffect(() => {
+    const getBlogData = async () => {
+      const data = await getBlogPostList();
+      setBlogPosts(data);
+    };
+    getBlogData();
+  }, [setBlogPosts]);
+
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.mainHeading}>
-        Latest Content:
-      </h1>
+      <h1 className={styles.mainHeading}>Latest Content:</h1>
 
-      {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
+      {blogPosts.map(({ slug, title, abstract, publishedOn }) => (
+        <BlogSummaryCard
+          key={publishedOn}
+          slug={slug}
+          title={title}
+          abstract={abstract}
+          publishedOn={publishedOn}
+        />
+      ))}
     </div>
   );
 }
